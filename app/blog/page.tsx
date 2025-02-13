0e638df5470e03b883a8d2d6a396e8155f91d3b1
code-savan <eric.marvelboy@gmail.com>
2025-02-13 16:25:40 +0100

    fixed vercel error

--- app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-const filteredBlogs = blogs.filter(blog => {
-  if (!searchTerm) return true
-  return (
-    blog.title.toLowerCase().includes(searchTerm) ||
-    blog.author.toLowerCase().includes(searchTerm) ||
-    (typeof blog.body === 'string' && blog.body.toLowerCase().includes(searchTerm)) ||
-    (typeof blog.body === 'object' && blog.body.content && typeof blog.body.content === 'string' && blog.body.content.toLowerCase().includes(searchTerm))
-  )
-})
